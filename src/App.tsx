5181e7b4936843907f9de3e6b880e32f3944fa47
Dimitar Zahariev <dbzahariev@gmail.com>
2024-03-11 18:33:06 +0200

    hi2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hi</p>
+        <p>hi2</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
